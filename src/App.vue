e80e38677f386bb583c93e38317d953fc837de7d
kawada <vgnsl94@gmail.com>
2021-01-24 17:00:40 +0900

    リストの作成

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,23 @@
     <v-app-bar color="primary" dark app clipped-left>
       <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Vuetify</v-toolbar-title>
-      <p>{{drawer}}</p>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text>For Enterprise</v-btn>
+        <v-menu offset-y>
+          <template v-slot:activator="{on}">
+          <v-btn v-on="on" text>Support</v-btn>
+          </template>
+          <v-list>
+            <v-subheader>Get help</v-subheader>
+            <v-list-item v-for="support in supports" :key="support">
+              <v-list-item-content>
+                <v-list-item-title>{{support}}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-toolbar-items>
     </v-app-bar>
     <v-footer color="primary" dark app>
       Vuetify
@@ -17,7 +33,14 @@
 export default {
   data() {
     return {
-      drawer: null
+      drawer: null,
+      supports:[
+          'Consulting and suppourt',
+          'Discord community',
+          'Report a bug',
+          'Github issue board',
+          'Stack overview'
+        ]
     }
   },
 
