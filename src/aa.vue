a33bca1480a4ad5be281e262c19047b9f19f9029
kawada <vgnsl94@gmail.com>
2021-02-01 17:21:21 +0900

    v-model

---
+++ src/aa.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+
+    <Title v-model="title" />
+    <v-app>
+      
+    </v-app>
+  </div>
+</template>
+
+
+
+<script>
+
+import Title from '@/Title.vue'
+
+
+  export default {
+    data () {
+      return {
+        select: { state: 'Florida', abbr: 'FL' },
+        items: [
+          { state: 'Florida', abbr: 'FL' },
+          { state: 'Georgia', abbr: 'GA' },
+          { state: 'Nebraska', abbr: 'NE' },
+          { state: 'California', abbr: 'CA' },
+          { state: 'New York', abbr: 'NY' },
+        ],
+        picker: '2020-06-04',
+        title: 'タイトル'
+      }
+    },
+    components: {Title}
+  }
+</script>
